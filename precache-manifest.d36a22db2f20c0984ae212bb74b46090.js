0193caecf1efc9ccb5f59360ef407e64268da67d
Himanshu Garg <himanshugarg@himanshu.garg>
2022-07-21 21:28:37 +0530

    Updates

---
+++ precache-manifest.d36a22db2f20c0984ae212bb74b46090.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e4923520b9b37a9601aa1d7972353f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7093d463ae8dbc8b6b50",
+    "url": "/static/css/2.a779dd76.chunk.css"
+  },
+  {
+    "revision": "b73d31184422805fe73b",
+    "url": "/static/css/main.2c27d62d.chunk.css"
+  },
+  {
+    "revision": "7093d463ae8dbc8b6b50",
+    "url": "/static/js/2.e0c782fa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e0c782fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b73d31184422805fe73b",
+    "url": "/static/js/main.b84d28e5.chunk.js"
+  },
+  {
+    "revision": "ee8c3b90c01e2a4b2e69",
+    "url": "/static/js/runtime-main.007aa73a.js"
+  },
+  {
+    "revision": "03b97d793ff9f17c2be2e52f218f228f",
+    "url": "/static/media/1.03b97d79.jpg"
+  },
+  {
+    "revision": "040c861dc8bc961557fbb67ed5ea7197",
+    "url": "/static/media/2.040c861d.jpg"
+  },
+  {
+    "revision": "39df94aeab903ab730124a43c3090289",
+    "url": "/static/media/CSS.39df94ae.png"
+  },
+  {
+    "revision": "c4d4583e328d5a72a4878eab33b07988",
+    "url": "/static/media/HTML.c4d4583e.jpg"
+  },
+  {
+    "revision": "6f11241f0d717b0c4e163986ba76fe0b",
+    "url": "/static/media/JS.6f11241f.png"
+  },
+  {
+    "revision": "50b1df9e47bc7fc08354ebf6e335fae5",
+    "url": "/static/media/Net.50b1df9e.png"
+  },
+  {
+    "revision": "22925b944293d116eb83ad56fcb334f8",
+    "url": "/static/media/angular.22925b94.png"
+  },
+  {
+    "revision": "61e3d5ce3923b1a74eab3434df097990",
+    "url": "/static/media/react.61e3d5ce.png"
+  }
+]);
